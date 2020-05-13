aaf01b0a608bfc1a9e19e61458b5a786fd32bc23
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-05-13 20:46:34 +0100

    Added enemy hit effects

--- Assets/Scripts/BulletCollisionHandler.cs
+++ Assets/Scripts/BulletCollisionHandler.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class BulletCollisionHandler : MonoBehaviour {
     [SerializeField] GameObject player;
+    [SerializeField] ParticleSystem bulletDestroyParticle;
     Collider playerCollider;
+    bool isActive = true;
     private void Start() {
         playerCollider = player.GetComponent<Collider>();
     }
+    private void Update() {
+        if (!isActive) {
+            //Disable renderer, collider, and rigid body, not trail.
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+            CircleCollider2D cc = GetComponent<CircleCollider2D>();
+            Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+            sr.enabled = false;
+            cc.enabled = false;
+            rb2d.velocity = new Vector2(0f, 0f);
+            rb2d.gravityScale = 0f;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.tag == "Enemy") {
-            print("Hit enemy");
+            ContactPoint2D col = collision.GetContact(0);
+            Vector2 pos = col.point;
+            Instantiate(bulletDestroyParticle, pos, Quaternion.identity);
+            isActive = false;
         }
     }
 }
---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+    [SerializeField] ParticleSystem onDeath;
+    [SerializeField] float recoverySpeed = 0.04f;
+    float lastHit = 0f; //Time since last time hit.
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "bullet(Clone)") {
+            lastHit = 0f;
+        }
+    }
+
+    private void Update() {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        float r = Mathf.Lerp(0, 1, lastHit);
+        float g = Mathf.Lerp(0, 1, lastHit);
+        float b = Mathf.Lerp(0, 1, lastHit);
+        Color currentColor = new Color(r, g, b);
+        renderer.color = currentColor;
+        lastHit = Mathf.Clamp(lastHit + recoverySpeed, 0, 1);
+    }
+}
