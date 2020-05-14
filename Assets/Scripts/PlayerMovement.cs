11925334ff6d45675a81913d9271a568f372f2df
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-05-14 18:28:12 +0100

    Added flash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     Rigidbody2D rb;
     [SerializeField] GameObject bullet;
+    [SerializeField] GameObject flashEffect;
     [SerializeField] ParticleSystem hitParticle;
+
     [SerializeField] float particleScale = 1000f;
     [SerializeField] float particleVelThresh = 20f; //If vel less than this no particles.
 
     [SerializeField] float speed = 10f;
     [SerializeField] float jumpSpeed = 300f;
     [SerializeField] float bulletVel = 100f;
+    [SerializeField] float flashDistance = 1.5f;
 
 
 
@@ -37,12 +41,16 @@ public class PlayerMovement : MonoBehaviour {
             Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mouse.z = 0;
             GameObject createdBullet = Instantiate(bullet, transform.position, Quaternion.identity);
-            float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) ;
+            float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x);
             float xVel = Mathf.Cos(angle) * bulletVel;
             float yVel = Mathf.Sin(angle) * bulletVel;
             createdBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(xVel, yVel);
         }
+
+        processFlash();
     }
+
+
     private void OnCollisionEnter2D(Collision2D collision) {
         ContactPoint2D contact = collision.GetContact(0);
         Vector2 pos = contact.point;
@@ -51,6 +59,21 @@ public class PlayerMovement : MonoBehaviour {
             Vector2 newScale = new Vector2(x, x);
             hitParticle.transform.localScale = newScale;
             Instantiate(hitParticle, pos, Quaternion.identity);
+        }
+    }
+
+    private void processFlash() {
+        if (Input.GetKeyDown("f")) {
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mouse2d = mouse;
+            Vector2 pos = transform.position;
+            float angle = Mathf.Atan2(mouse2d.y - pos.y, mouse2d.x - pos.x);
+            float x = Mathf.Cos(angle) * flashDistance;
+            float y = Mathf.Sin(angle) * flashDistance;
+
+            Instantiate(flashEffect, transform.position, Quaternion.identity);
+            transform.position = new Vector2(pos.x + x, pos.y + y);
+            Instantiate(flashEffect, transform.position, Quaternion.identity);
         }
     }
 }
